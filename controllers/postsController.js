456a9b161801f0dfc35fd2648bab347f3c40e327
MyUsername <rosanio.c@gmail.com>
2024-11-21 13:04:09 +0100

    CONTROLLERS creazione file + struttura base

---
+++ controllers/postsController.js
@@ -0,0 +1,51 @@
+// Dichiarazione dei REQUIRE delle risorse
+const path = require('path');
+const postArray = require('../data/posts.js');
+
+
+
+// index
+function index() {
+
+}
+
+
+// show
+function show() {
+
+}
+
+
+// store
+function store() {
+
+}
+
+
+// update
+function update() {
+
+}
+
+
+// modify
+function modify() {
+
+}
+
+
+// destroy
+function destroy() {
+
+}
+
+
+
+module.exports = {
+    index,
+    show,
+    store,
+    update,
+    modify,
+    destroy,
+}

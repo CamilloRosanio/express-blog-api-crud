33c3c3f0c6bd3c0341e1b7ea14848914346a60e8
MyUsername <rosanio.c@gmail.com>
2024-11-22 09:57:29 +0100

    CORREZIONE 1 - centralizzazione PATH immagini dei Post

--- data/posts.js
+++ data/posts.js
@@ -1,8 +1,8 @@
-// Per comodità e centralizzazione faccio il REQUIRE del .env così da avere i Link delle immagini dinamici
+// Per comodità centralizzo il PATH delle immagini (ad esempio)
 
-const dotEnv = require('dotenv').config();
 const port = process.env.PORT;
 const host = process.env.HOST;
+const folderPath = `/img/`;
 
 
 
@@ -11,35 +11,35 @@ const postArray = [
         id: 2,
         title: 'Ciambellone',
         content: 'Delizioso ciambellone',
-        img: host + port + `/img/ciambellone.jpeg`,
+        img: host + port + folderPath + `ciambellone.jpeg`,
         tags: ['ciambellone', 'Dolce', 'Torte']
     },
     {
         id: 4,
         title: 'Cracker di barbabietola',
         content: 'Preparazione cracker di barbabietola',
-        img: host + port + `/img/cracker_barbabietola.jpeg`,
+        img: host + port + folderPath + `cracker_barbabietola.jpeg`,
         tags: ['barbabietola', 'Cracker', 'impasto', 'salato']
     },
     {
         id: 6,
         title: 'Pane fritto',
         content: 'Come fare il pane fritto',
-        img: host + port + `/img/pane_fritto_dolce.jpeg`,
+        img: host + port + folderPath + `pane_fritto_dolce.jpeg`,
         tags: ['dolce', 'Pane fritto']
     },
     {
         id: 8,
         title: 'Pasta di barbabietola',
         content: 'Come preparare la pasta alla barbabietola',
-        img: host + port + `/img/pasta_barbabietola.jpeg`,
+        img: host + port + folderPath + `pasta_barbabietola.jpeg`,
         tags: ['barbabietola', 'pasta', 'impasto', 'salato']
     },
     {
         id: 10,
         title: 'Torta paesana',
         content: '',
-        img: host + port + `/img/torta_paesana.jpeg`,
+        img: host + port + folderPath + `torta_paesana.jpeg`,
         tags: ['torte', 'torta paesana', 'dolce']
     }
 ]

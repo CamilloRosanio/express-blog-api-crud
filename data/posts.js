bd8ba73f926bfc6024c26a778d9c9c26f61a4c14
MyUsername <rosanio.c@gmail.com>
2024-11-22 10:00:06 +0100

    img PATH dinamico

--- data/posts.js
+++ data/posts.js
@@ -11,35 +11,35 @@ const postArray = [
         id: 2,
         title: 'Ciambellone',
         content: 'Delizioso ciambellone',
-        img: host + port + folderPath + `ciambellone.jpeg`,
+        img: `${host}:${port}${folderPath}ciambellone.jpeg`,
         tags: ['ciambellone', 'Dolce', 'Torte']
     },
     {
         id: 4,
         title: 'Cracker di barbabietola',
         content: 'Preparazione cracker di barbabietola',
-        img: host + port + folderPath + `cracker_barbabietola.jpeg`,
+        img: `${host}:${port}${folderPath}cracker_barbabietola.jpeg`,
         tags: ['barbabietola', 'Cracker', 'impasto', 'salato']
     },
     {
         id: 6,
         title: 'Pane fritto',
         content: 'Come fare il pane fritto',
-        img: host + port + folderPath + `pane_fritto_dolce.jpeg`,
+        img: `${host}:${port}${folderPath}pane_fritto_dolce.jpeg`,
         tags: ['dolce', 'Pane fritto']
     },
     {
         id: 8,
         title: 'Pasta di barbabietola',
         content: 'Come preparare la pasta alla barbabietola',
-        img: host + port + folderPath + `pasta_barbabietola.jpeg`,
+        img: `${host}:${port}${folderPath}pasta_barbabietola.jpeg`,
         tags: ['barbabietola', 'pasta', 'impasto', 'salato']
     },
     {
         id: 10,
         title: 'Torta paesana',
         content: '',
-        img: host + port + folderPath + `torta_paesana.jpeg`,
+        img: `${host}:${port}${folderPath}torta_paesana.jpeg`,
         tags: ['torte', 'torta paesana', 'dolce']
     }
 ]
